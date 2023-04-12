d0e2c037efa5b614ad89d965e1492e3191bf3213
DaSaltySquid <104535317+DaSaltySquid@users.noreply.github.com>
2023-04-12 11:35:06 -0400

    Added Shopping List Class

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milk");
+        System.out.println("Ham");
+        System.out.println("Cheese");
+        System.out.println("Bread");
+        System.out.println("Mayo");
+        System.out.println("Ketchup");
+        System.out.println("Mustard");
+        System.out.println("Lettuce");
+        System.out.println("Tomato");
+        System.out.println("Juice");
+    }
+}
